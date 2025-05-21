bc50c354768fc9635f3d73db6dc5d3de784ae6da
gpapachr <giannis.papachristou@yahoo.gr>
2025-05-21 17:02:32 +0300

    Fix formatting issues in main and test classes

--- src/main/java/gr/seo/youthapp/YouthAppApplication.java
+++ src/main/java/gr/seo/youthapp/YouthAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YouthAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YouthAppApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(YouthAppApplication.class, args);
+  }
 }
