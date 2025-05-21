0b4204188c819ed0c418c20fe6c272e0626751c9
gpapachr <giannis.papachristou@yahoo.gr>
2025-05-21 14:28:26 +0300

    Initial commit

---
+++ src/main/java/gr/seo/youthapp/YouthAppApplication.java
@@ -0,0 +1,13 @@
+package gr.seo.youthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/seo/youthapp/YouthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.seo.youthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
