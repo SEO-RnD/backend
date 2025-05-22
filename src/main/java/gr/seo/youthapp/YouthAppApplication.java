66de9f6c9987b9a331c0d6bcb2b5cb1e6f422c2f
gpapachr <giannis.papachristou@yahoo.gr>
2025-05-22 12:57:33 +0300

    Add documentation to YouthAppApplication class and main method

--- src/main/java/gr/seo/youthapp/YouthAppApplication.java
+++ src/main/java/gr/seo/youthapp/YouthAppApplication.java
@@ -3,9 +3,19 @@ package gr.seo.youthapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The YouthAppApplication class serves as the entry point for the Spring Boot application. It
+ * initializes and runs the application using the SpringApplication.run method.
+ */
 @SpringBootApplication
 public class YouthAppApplication {
 
+  /**
+   * The main method serves as the entry point of the application. It uses Spring Boot's
+   * SpringApplication.run method to bootstrap the application.
+   *
+   * @param args command-line arguments passed to the application
+   */
   public static void main(String[] args) {
     SpringApplication.run(YouthAppApplication.class, args);
   }
