6d211ae7b26d4fa24ae62b9214982c21745cca02
gpapachr <giannis.papachristou@yahoo.gr>
2025-09-26 21:39:06 +0300

    chore(init): add initial Spring Boot project structure

---
+++ src/main/java/gr/seo/guides/backend/SeoGuidesBackendApplication.java
@@ -0,0 +1,13 @@
+package gr.seo.guides.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoGuidesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoGuidesBackendApplication.class, args);
+	}
+
+}
